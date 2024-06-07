3b2a89f5ac3bfec24333b1f67032681302062a47
Ashyleuy <150076417+AshleighSham@users.noreply.github.com>
2024-06-07 18:01:55 +0100

    function doc strings

--- utilities.py
+++ utilities.py
@@ -2,10 +2,18 @@ import numpy as np
 
 def forward_model(x):
 
-    
+
 
 def check_bounds(x, range):
+    """Check that the proposed thetas are within the defined ranges.
 
+        Args:
+            measurement (numpy.array): np.array([E, G_f]), np.array([[E_min, G_min],[E_max, G_max]])
+
+        Returns:
+            numpy.array: np.array([E, G_f]) with values previously falling outside of the range replaced with the bounds
+    """
+    
     mini = range[0]
     maxi = range[1]
 
@@ -15,7 +23,14 @@ def check_bounds(x, range):
     return x
 
 def ESS(measurements, e):
+    """Calculates EES results of FEM with etsimated E, G_f and the measured data
 
+        Args:
+            measurement (numpy.array): np.array([observations]), np.array([E, G_f])
+
+        Returns:
+            numpy.array: ESS, resulting FEM
+    """
     arr = forward_model(e)
     ss1 = np.linalg.norm(measurements - arr)
 
